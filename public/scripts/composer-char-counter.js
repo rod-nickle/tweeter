367292d8b6150f99b571046b49af84f73fed465b
rod-nickle <rod@bitwise-it.com>
2024-11-06 16:01:30 -0700

    Add section for old tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
 
-  // As characters are entered into the input field, 
+  // As characters are entered into the input field,
   // update the counter with the number of characters remaining.
   // The maximum length is 140 chars.  Change the counter color
   // to red when the 140 char limit is exceeded.
-  $("#tweet-text").on("input", function(event) {
-    const counterElement = "output[name='counter']";
+  $("#tweet-text").on("input", function() {
+    //const counterElement = "output[name='counter']";
     const tweetText = $(this).val();
     const tweetTextLength = tweetText.length;
     const tweetTextCharsRemaining = 140 - tweetTextLength;
   
-    if ($(counterElement)) {
-      $(counterElement).val(tweetTextCharsRemaining);
+    let $form = $(this).closest("form");
+    let $counter = $form.find("output[name='counter']");
+    if ($counter) {
+      $counter.val(tweetTextCharsRemaining);
       // This class will change the counter color to red.
       if (tweetTextCharsRemaining < 0) {
-        $(counterElement).addClass("counter-negative");
+        $counter.addClass("counter-negative");
       } else {
-        $(counterElement).removeClass("counter-negative");
+        $counter.removeClass("counter-negative");
       }
     }
   });
-
 });
-
 
 
 
