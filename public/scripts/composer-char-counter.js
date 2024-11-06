4c2251eb1ed2a7f1dee339ffeb69a57593b35465
rod-nickle <rod@bitwise-it.com>
2024-11-06 10:14:29 -0700

    Clean up composer-char-counter.js after Larry AI feedback.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
   // The maximum length is 140 chars.  Change the counter color
   // to red when the 140 char limit is exceeded.
   $("#tweet-text").on("input", function(event) {
+    const counterElement = "output[name='counter']";
     const tweetText = $(this).val();
     const tweetTextLength = tweetText.length;
     const tweetTextCharsRemaining = 140 - tweetTextLength;
   
-    const counter = document.querySelector("output[name='counter']");
-    if (counter) {
-      counter.value = tweetTextCharsRemaining;
-      counter.className = "counter";
+    if ($(counterElement)) {
+      $(counterElement).val(tweetTextCharsRemaining);
       // This class will change the counter color to red.
       if (tweetTextCharsRemaining < 0) {
-        counter.className = "counter-negative";
+        $(counterElement).addClass("counter-negative");
+      } else {
+        $(counterElement).removeClass("counter-negative");
       }
     }
   });
