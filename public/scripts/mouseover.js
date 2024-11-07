2302858fc0361ec3d170cefc717d8ab32a67b174
rod-nickle <rod@bitwise-it.com>
2024-11-06 17:02:00 -0700

    change to use :hover pseudo-class instead of event listener based on feedback from Larry AI

--- public/scripts/mouseover.js
+++ public/scripts/mouseover.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
 
-    $('.old-tweet-section').on('mouseover', function () {
-      $(this).addClass("old-tweet-section-mouseover");
-    }).on('mouseout', function () {
-      $(this).removeClass("old-tweet-section-mouseover");
-    });
+    // $('.old-tweet-section').on('mouseover', function () {
+    //   $(this).addClass("old-tweet-section-mouseover");
+    // }).on('mouseout', function () {
+    //   $(this).removeClass("old-tweet-section-mouseover");
+    // });
 
-    $('.hover-button').on('mouseover', function () {
-      $(this).addClass("button-mouseover");
-    }).on('mouseout', function () {
-      $(this).removeClass("button-mouseover");
-    });
+    // $('.hover-button').on('mouseover', function () {
+    //   $(this).addClass("button-mouseover");
+    // }).on('mouseout', function () {
+    //   $(this).removeClass("button-mouseover");
+    // });
 });
 
 
